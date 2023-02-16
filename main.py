56b7cbeec354f4d52a61b4d13bef747e2c974c31
pahakahapaul <ponompaul@icloud.com>
2023-02-16 17:34:55 +0200

    calc
    add function

---
+++ main.py
@@ -0,0 +1,18 @@
+print('введите число а=')
+a = int (input())
+
+print('введите число б=')
+b = int (input())
+
+print('выберете операцию')
+print('1 - сложить a+b')
+print('2 - вычесть a-b')
+d = int (input())
+
+
+if d==1:
+  print ('сумма a+b=',a+b)
+
+
+if d==2:
+  print ('разность a-b=',a-b)
